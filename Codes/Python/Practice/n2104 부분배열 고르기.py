ac651457bd301563c8af5869f24925679a282d66
dkqyqyt <rsm4282@naver.com>
2020-06-04 16:43:28 +0900

    Add 부분배열고르기

--- Codes/Python/Practice/n2104 부분배열 고르기.py
+++ Codes/Python/Practice/n2104 부분배열 고르기.py
@@ -2,52 +2,39 @@ def find_arr(s,e):
     if s == e:
         return 0
     if s + 1 == e:
-        return graph[s]
+        return graph[s]**2
 
     mid = (s+e)//2
-    result = max(find_arr(s,mid), find_arr(mid,e))
+    result = max(find_arr(s,mid), find_arr(mid,e),graph[mid]**2)
 
     min_num = graph[mid]
     mysum = graph[mid]
     l = mid
     r = mid
-    mid_result = mysum*min_num
 
-    while l > s:
-        l -= 1
-        if graph[l] < min_num:
-            temp = min(graph[l],min_num) * (mysum+graph[l])
-            if temp > mid_result:
-                mysum += graph[l]
-                min_num = min(graph[l],min_num)
-                mid_result = mysum * min_num
-            else:
-                l += 1
-                break
+    while r-l+1 < e-s:
+        if l-1 >= s:
+            p = min(min_num,graph[l-1])
         else:
-            mysum += graph[l]
-            mid_result = mysum * min_num
-    while r < e-1:
-        r += 1
-        if graph[r] < min_num:
-            temp = min(graph[r],min_num) * (mysum+graph[r])
-            if temp > mid_result:
-                mysum += graph[r]
-                min_num = min(graph[r],min_num)
-                mid_result = mysum * min_num
-            else:
-                r -= 1
-                break
+            p = 0
+        if r+1 < e:
+            q = min(min_num, graph[r+1])
         else:
-            mysum += graph[r]
-            mid_result = mysum * min_num
+            q = 0
 
-    # print('s,e : ', s, e)
-    # print('l,r: ', l , r)
-    # print('result: ', result)
-    # print('mid_result: ', mid_result)
+        if p >= q:
+            min_num = p
+            mysum += graph[l-1]
+            result = max(result, mysum*min_num)
+            l -= 1
+        else:
+            min_num = q
+            mysum += graph[r+1]
+            result = max(result, mysum*min_num)
+            r += 1
 
-    return max(result, mid_result)
+    return result
+
 n = int(input())
 
 graph = list(map(int,input().split()))
